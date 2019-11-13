3aaa720a132b2e0cf5fb9f7247de964a19016a67
Guilherme Nogueira <guilherme.nogueira@engdb.com.br>
2019-11-13 15:16:23 -0300

    teste

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "git diff --cached --name-status | cut -f2"
+
--- script2.py
+++ script2.py
@@ -4,12 +4,20 @@ import os
 
 def create():
 
-    commit_hash = os.popen("git log -n 1 master --pretty=format:\"%H\"").read()
-    msg = """
-    git show --pretty=\"format:\" --name-only {commit_hash}\""
-    """.format(commit_hash=commit_hash)
-    sys_msg = os.system(msg)
-    return sys_msg
+   # commit_hash = os.popen("git log -n 1 master --pretty=format:\"%H\"").read()
+   # msg = """
+   # git show --pretty=\"format:\" --name-only {commit_hash}\""
+   # """.format(commit_hash=commit_hash)
+   # sys_msg = os.system(msg)
+   # return sys_msg
+
+   teste = os.popen("git diff --cached --name-status | cut -f2").read().splitlines()
+   print (teste)
+
+   for i in range (len(teste)):
+   	path = os.popen(" readlink -f "+teste[i]).read()
+   	print(path)
 
 if  __name__ == '__main__':
     create()
+

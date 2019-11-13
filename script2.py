85c274de4526a32490c15476db5f59a7db32d36d
Guilherme Nogueira <guilherme.nogueira@engdb.com.br>
2019-11-13 14:23:05 -0300

    teste

--- script2.py
+++ script2.py
@@ -2,9 +2,7 @@
 
 import os
 
-
 def create():
-    """Get the hash of the last commited commit"""
 
     commit_hash = os.popen("git log -n 1 master --pretty=format:\"%H\"").read()
     msg = """
@@ -12,7 +10,6 @@ def create():
     """.format(commit_hash=commit_hash)
     sys_msg = os.system(msg)
     return sys_msg
-
 
 if  __name__ == '__main__':
     create()

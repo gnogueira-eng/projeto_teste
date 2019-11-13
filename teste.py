481e9cd2bc043acd133ccf856c3a2011ba21f605
Guilherme Nogueira <guilherme.nogueira@engdb.com.br>
2019-11-13 11:08:46 -0300

    outro script

---
+++ teste.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import subprocess
+
+#svnlook location
+LOOK='svnlook'
+
+
+def get_commit_info( repo, revision ):
+	log = svn_look('log', repo, '-r', revision)
+	author = svn_look('author', repo, '-r', revision)
+	repo_name = repo.split('/')
+	payload = {'revision' : 'repo: ' + repo_name[-1] + '\nrevision: ' + revision, 
+	'log' : 'commit message: \n' + log, 
+	'author': '\nauthor: ' + author}
+	return payload
+
+def svn_look( *args ):
+	p = subprocess.Popen(' '.join([LOOK] + list(args)), stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT )
+	out, err = p.communicate()
+	return out
+
+if __name__ == '__main__':
+	main(sys.get_commit_info)
